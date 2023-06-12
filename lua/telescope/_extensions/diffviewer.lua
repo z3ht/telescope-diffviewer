31e497ddfd6cdfc2cb2d03d7bb1112e8b568a8bb
Andrew Darling <andr3w.darling@gmail.com>
2023-06-11 21:05:09 -0500

    Minimal

--- lua/telescope/_extensions/diffviewer.lua
+++ lua/telescope/_extensions/diffviewer.lua
@@ -1,7 +1,7 @@
 local builtin = require('telescope.builtin')
 
 local function apply_diffviewer(_, _)
-   builtin.git_commits {
+   builtin.find_files {
       attach_mappings = function(_, map)
           map({"i", "n"}, "<C-r>", function(_prompt_bufnr)
               print "You typed <C-r>"
