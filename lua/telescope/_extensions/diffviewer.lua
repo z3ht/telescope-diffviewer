edb3a6c1411bd8617db348e55b93f26439b8aef0
Andrew Darling <andr3w.darling@gmail.com>
2023-06-11 23:09:05 -0500

    aklsdf

--- lua/telescope/_extensions/diffviewer.lua
+++ lua/telescope/_extensions/diffviewer.lua
@@ -1,15 +1,11 @@
 local function apply_diffviewer(cfg, ext_cfg)
-    require('telescope').defaults.mappings {
-        pickers = {
-            find_files = {
-                mappings = {
+    require('telescope.builtin').git_commits({
+                attach_mappings = {
                     n = { ["<C-r>d"] = function(_prompt_bufnr)
-                        print "You typed <C-r>"
+                        print "You typed <C-r>d"
                     end}
                 }
-            }
-        }
-    }
+    })
 end
 
 return {
