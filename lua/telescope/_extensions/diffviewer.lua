89bd5d241e9edb50e075667d8fcdb7938c48c804
Andrew Darling <andr3w.darling@gmail.com>
2023-06-11 23:35:05 -0500

    revert to happy version... finally

--- lua/telescope/_extensions/diffviewer.lua
+++ lua/telescope/_extensions/diffviewer.lua
@@ -1,14 +1,38 @@
-local function apply_diffviewer(cfg, ext_cfg)
-    require('telescope.builtin').git_commits({
-                attach_mappings = {
-                    n = { ["<C-r>d"] = function(_prompt_bufnr)
-                        print "You typed <C-r>d"
-                    end}
-                }
-    })
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+local utils = require "telescope.utils"
+
+
+local function diffview_commits(prompt_bufnr)
+  local picker = action_state.get_current_picker(prompt_bufnr)
+  local selection = picker:get_selected_entry()
+  if selection == nil then
+    utils.__warn_no_selection "extensions.diffviewer.commit_diffview"
+    return
+  end
+
+  actions.close(prompt_bufnr)
+
+  print("commit_diffview "..selection.value)
+end
+
+local function diffview_branches(prompt_bufnr)
+  local picker = action_state.get_current_picker(prompt_bufnr)
+  local selection = picker:get_selected_entry()
+  if selection == nil then
+    utils.__warn_no_selection "extensions.diffviewer.commit_diffview"
+    return
+  end
+
+  actions.close(prompt_bufnr)
+
+  print("branch_diffview "..selection.value)
 end
 
 return {
-    setup = apply_diffviewer
+    exports = {
+        commits = diffview_commits,
+        branches = diffview_branches
+    }
 }
 
