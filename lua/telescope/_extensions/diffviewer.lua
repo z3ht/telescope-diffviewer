e8d8297fff103442847dd0bf956fd28c88bc3e06
Andrew Darling <andr3w.darling@gmail.com>
2023-06-11 21:57:28 -0500

    fix bug

--- lua/telescope/_extensions/diffviewer.lua
+++ lua/telescope/_extensions/diffviewer.lua
@@ -2,7 +2,7 @@ local builtin = require('telescope.builtin')
 
 local function apply_diffviewer(_, _)
    builtin.find_files {
-      attach_mappings = function(_, map)
+      mappings = function(_, map)
           map({"i", "n"}, "<C-r>", function(_prompt_bufnr)
               print "You typed <C-r>"
           end)
