7c3624c2e3d4e50af35c8755349f568143b67fe0
Andrew Darling <andr3w.darling@gmail.com>
2023-06-11 19:38:58 -0500

    partial implementation

---
+++ lua/telescope/_extensions/diffviewer/diffviewer.lua
@@ -0,0 +1,79 @@
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+local utils = require "telescope.utils"
+
+
+local function diffview(target)
+    vim.cmd.DiffviewOpen{target}
+end
+
+local function commit_diffview(prompt_bufnr)
+  local picker = action_state.get_current_picker(prompt_bufnr)
+  local selection = picker:get_selected_entry()
+  if selection == nil then
+    utils.__warn_no_selection "extensions.diffviewer.commit_diffview"
+    return
+  end
+
+  actions.close(prompt_bufnr)
+
+  print("commit_diffview "..selection.value)
+end
+
+local function branch_diffview(prompt_bufnr)
+  local picker = action_state.get_current_picker(prompt_bufnr)
+  local selection = picker:get_selected_entry()
+  if selection == nil then
+    utils.__warn_no_selection "extensions.diffviewer.commit_diffview"
+    return
+  end
+
+  actions.close(prompt_bufnr)
+
+  print("branch_diffview "..selection.value)
+end
+
+local function stash_diffview(prompt_bufnr)
+  local picker = action_state.get_current_picker(prompt_bufnr)
+  local selection = picker:get_selected_entry()
+  if selection == nil then
+    utils.__warn_no_selection "extensions.diffviewer.commit_diffview"
+    return
+  end
+
+  actions.close(prompt_bufnr)
+
+  print("stash_diffview "..selection.value)
+end
+
+local telescope = require('telescope')
+
+local function apply_diffviewer(_, _)
+    telescope.git_commits {
+        attach_mappings = function(_, map)
+            map({"i", "n"}, "<C-r>d", commit_diffview)
+        end
+    }
+    telescope.git_branches {
+        attach_mappings = function(_, map)
+            map({"i", "n"}, "<C-r>d", branch_diffview)
+        end
+    }
+    telescope.git_stash {
+        attach_mappings = function(_, map)
+            map({"i", "n"}, "<C-r>d", stash_diffview)
+        end
+    }
+end
+
+return {
+    setup = apply_diffviewer,
+    exports = {
+        diffview = diffview,
+        commit_diffview = commit_diffview,
+        branch_diffview = branch_diffview,
+        stash_diffview = stash_diffview
+    }
+}
+
+
