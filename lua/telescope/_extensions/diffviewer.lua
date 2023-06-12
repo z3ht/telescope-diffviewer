eb6cfaacd5cbde08354d5d9e22455f601a47f9b0
Andrew Darling <andr3w.darling@gmail.com>
2023-06-11 20:58:11 -0500

    minimal

--- lua/telescope/_extensions/diffviewer.lua
+++ lua/telescope/_extensions/diffviewer.lua
@@ -16,50 +16,12 @@ local function commit_diffview(prompt_bufnr)
   print("commit_diffview "..selection.value)
 end
 
-local function branch_diffview(prompt_bufnr)
-  local picker = action_state.get_current_picker(prompt_bufnr)
-  local selection = picker:get_selected_entry()
-  if selection == nil then
-    utils.__warn_no_selection "extensions.diffviewer.commit_diffview"
-    return
-  end
-
-  actions.close(prompt_bufnr)
-
-  print("branch_diffview "..selection.value)
-end
-
-local function stash_diffview(prompt_bufnr)
-  local picker = action_state.get_current_picker(prompt_bufnr)
-  local selection = picker:get_selected_entry()
-  if selection == nil then
-    utils.__warn_no_selection "extensions.diffviewer.commit_diffview"
-    return
-  end
-
-  actions.close(prompt_bufnr)
-
-  print("stash_diffview "..selection.value)
-end
-
 local builtin = require('telescope.builtin')
 
 local function apply_diffviewer(_, _)
     builtin.git_commits {
         attach_mappings = function(_, map)
-            map("n", "gv", commit_diffview)
-            return true
-        end
-    }
-    builtin.git_branches {
-        attach_mappings = function(_, map)
-            map("n", "gv", branch_diffview)
-            return true
-        end
-    }
-    builtin.git_stash {
-        attach_mappings = function(_, map)
-            map("n", "gv", stash_diffview)
+            map({"n", "i"}, "gv", commit_diffview)
             return true
         end
     }
