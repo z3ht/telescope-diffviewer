a37608fb47689c952367c7456c8a92f3c94d9828
Andrew Darling <andr3w.darling@gmail.com>
2023-06-11 22:51:02 -0500

    back to the depths

--- lua/telescope/_extensions/diffviewer.lua
+++ lua/telescope/_extensions/diffviewer.lua
@@ -1,15 +1,19 @@
 local function apply_diffviewer(cfg, ext_cfg)
-    require('telescope').setup {
-        pickers = {
-            find_files = {
-                mappings = {
-                    n = { ["<C-r>d"] = function(_prompt_bufnr)
-                        print "You typed <C-r>"
-                    end}
-                }
-            }
-        }
-    }
+    require("telescope.builtin").find_files {
+     attach_mappings = function(_, map)
+       map("i", "asdf", function(_prompt_bufnr)
+         print "You typed asdf"
+       end)
+
+       map({"i", "n"}, "<C-r>", function(_prompt_bufnr)
+         print "You typed <C-r>"
+       end)
+
+       -- needs to return true if you want to map default_mappings and
+       -- false if not
+       return true
+     end,
+   }
 end
 
 return {
