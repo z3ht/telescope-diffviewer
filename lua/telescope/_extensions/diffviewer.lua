611a99ca5d5724f1d46aef44beb1c94955a9aedd
Andrew Darling <andr3w.darling@gmail.com>
2023-06-11 23:49:04 -0500

    simplify

--- lua/telescope/_extensions/diffviewer.lua
+++ lua/telescope/_extensions/diffviewer.lua
@@ -3,19 +3,7 @@ local action_state = require('telescope.actions.state')
 local utils = require "telescope.utils"
 
 
-local function diffview_commits(prompt_bufnr)
-  local selection = action_state.get_selected_entry()
-  if selection == nil then
-    utils.__warn_no_selection "extensions.diffviewer.commit_diffview"
-    return
-  end
-
-  actions.close(prompt_bufnr)
-
-  vim.cmd.DiffviewOpen{selection.value}
-end
-
-local function diffview_branches(prompt_bufnr)
+local function diffview(prompt_bufnr)
   local selection = action_state.get_selected_entry()
   if selection == nil then
     utils.__warn_no_selection "extensions.diffviewer.commit_diffview"
@@ -29,8 +17,8 @@ end
 
 return {
     exports = {
-        commits = diffview_commits,
-        branches = diffview_branches
+        commits = diffview,
+        branches = diffview
     }
 }
 
