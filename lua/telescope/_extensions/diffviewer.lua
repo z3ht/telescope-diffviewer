1acf1d56b3bd31d913a7417bfefb7785ebb6f90f
Andrew Darling <andr3w.darling@gmail.com>
2023-06-11 22:31:00 -0500

    plz work pt2

--- lua/telescope/_extensions/diffviewer.lua
+++ lua/telescope/_extensions/diffviewer.lua
@@ -1,12 +1,15 @@
 local function apply_diffviewer(cfg, ext_cfg)
-   require('telescope.builtin').find_files {
-      attach_mappings = function(_, map)
-          map({"i", "n"}, "<C-r>d", function(_prompt_bufnr)
-              print "You typed <C-r>"
-          end)
-          return true
-      end,
-  }
+    require('telescope').setup {
+        pickers = {
+            find_files = {
+                mappings = {
+                    n = { ["<C-r>d"] = function(_prompt_bufnr)
+                        print "You typed <C-r>"
+                    end}
+                }
+            }
+        }
+    }
 end
 
 return {
